8bd58a662b26caa89ad43f152f85901b3e1cc165
wang yi <756470059@qq.com>
2020-07-27 21:45:08 +0800

    add p2 Singleton

---
+++ src/main/java/com/yiwang/javainterview/atguiguphase1/SingletonP2.java
@@ -0,0 +1,12 @@
+package com.yiwang.javainterview.atguiguphase1;
+
+public class SingletonP2 {
+    private static final SingletonP2 single = new SingletonP2();
+
+    private SingletonP2() {
+    }
+
+    public SingletonP2 getInstance() {
+        return single;
+    }
+}
