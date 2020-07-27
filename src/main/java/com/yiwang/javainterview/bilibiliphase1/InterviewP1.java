9080bef1cfbb5ac6cd617b659f4759805d01bfdd
wang yi <756470059@qq.com>
2020-07-27 21:17:59 +0800

    add package interview

---
+++ src/main/java/com/yiwang/javainterview/bilibiliphase1/InterviewP1.java
@@ -0,0 +1,14 @@
+package com.yiwang.javainterview.bilibiliphase1;
+
+public class InterviewP1 {
+    public static void main(String[] args) {
+        int i = 1;
+        i = i++;    //i++ 是先把值压入操作数栈 再执行递增计算。这里 i = 1；
+        int j = i++;    //i = 2 , j = 1；
+        int k = i + ++i * i++; //2 + 3 * 3
+
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
+    }
+}
